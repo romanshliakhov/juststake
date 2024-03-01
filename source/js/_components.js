9cdcca6ec619ec141e4b60e7eed5ec79b8910ee1
Roman Shliakhov <romanshliakhov@gmail.com>
2024-03-01 04:39:27 +0200

    update files

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,7 +1,7 @@
 import './components/select.js';
 // import './components/sliders.js';
 import './components/burger.js';
-// import './components/spollers.js';
+import './components/spollers.js';
 import './components/mobileMenu.js';
 // import './components/replaceEl.js';
 // import './components/seoBlock.js';
